ef32d9adc7dc0c4d4de10c1b42321f084cb674d0
Shoham Sen <digo.sen16@gmail.com>
2024-03-19 14:39:48 -0500

    merged Energymin and Energymin-Version2. Made reading in data easy. Now just 1 fn space, V.

--- Python/PhaseTransition/GinzburgLandau/2D/EnergyMin.py
+++ Python/PhaseTransition/GinzburgLandau/2D/EnergyMin.py
@@ -1,6 +1,10 @@
 #Here we solve the 2D Ginzbug Landau problem with an applied magnetic field.
 #Here we want to use Energy minimization method. We start off with Gradient Descent.
 #HEre a1 is \ve{A}\cdot e_1, a2 is \ve{A}\cdot e_2, u is u. However, \theta=t
+#------------------------------------------------------------------------------------------------------
+# For details on progress, visit the overleaf file:-
+#1. Overleaf. superconductivity-Pradeep+Liping/Z3-Coding.tex/Sec. Stochastic Energy minimization methods
+# /subsec. Gradient Descent in FEniCS/paragraph{Wrote a 2D Ginzburg LAndau Energy minimization code}
 #======================================================================================================
 #The way the Code works
 #1. The input to the code is:
@@ -22,7 +26,6 @@
 #======================================================================================================
 #ISSUES WITH THE CODE:-
 
-
 import dolfin
 print(f"DOLFIN version: {dolfin.__version__}")
 from dolfin import *
@@ -41,35 +44,32 @@ ly = 10
 kappa = Constant(2.0)
 mesh = RectangleMesh(Point(0., 0.), Point(lx, ly), np.ceil(lx*10/kappa), np.ceil(ly*10/kappa), "crossed")
 x = SpatialCoordinate(mesh)
-Va1 = FiniteElement("CG", mesh.ufl_cell(), 2)
-Va2 = FiniteElement("CG", mesh.ufl_cell(), 2)
-Vt = FiniteElement("CG", mesh.ufl_cell(), 2)
-Vu = FiniteElement("CG", mesh.ufl_cell(), 2)
-V = FunctionSpace(mesh, MixedElement(Va1, Va2, Vt, Vu))
-Vcoord = FunctionSpace(mesh, "Lagrange", 2)#This is for ExtFile
+V = FunctionSpace(mesh, "Lagrange", 2)#This is for ExtFile
 
 
 # Define functions
-a1a2tu = Function(V)
-(a1, a2, t, u) = split(a1a2tu)
-a1a2tu_up = Function(V)
-(a1_up, a2_up, t_up, u_up) = split(a1a2tu_up)
+a1 = Function(V)
+a2 = Function(V)
+t = Function(V)
+u = Function(V)
+a1_up = Function(V)
+a2_up = Function(V)
+t_up = Function(V)
+u_up = Function(V)
 
 # Parameters
-gamma = float(0.01) # Learning rate.
-NN = int(input('Number of iterations?\n')) # Number of iterations
-Hin = input("External Magnetic field? ")
-H = Constant(Hin);
-tol_in = input("absolute tolerance? ")
-tol = Constant(tol_in);
+gamma = float(input('Learning rate?')) # Learning rate.
+NN = int(input('Number of iterations?')) # Number of iterations
+H = Constant(input("External Magnetic field? "));
+tol = float(input("absolute tolerance?"))
 Ae = H*x[0] #The vec pot is A(x) = Hx_1e_2
-
+read_in = int(input("Read from file? 1 for Yes, 0 for No"))
 
 def curl(a1,a2):
-    return a1.dx(0) - a2.dx(1)
+    return a2.dx(0) - a1.dx(1)
 
 #Defining the energy
-Pi = ((1-u**2)**2/2 + (1/kappa**2)*inner(grad(u), grad(u)) + ( (a1-t.dx(0))**2 + (a2-t.dx(1))**2 )*u**2 \
+Pi = ( (1-u**2)**2/2 + (1/kappa**2)*inner(grad(u), grad(u)) + ( (a1-t.dx(0))**2 + (a2-t.dx(1))**2 )*u**2 \
       + inner( curl(a1 ,a2-Ae), curl(a1 ,a2-Ae) ) )*dx
 
 #Defining the gradient
@@ -80,42 +80,40 @@ Fu = derivative(Pi, u)
 
 
 ##Setting up the initial conditions
-##SC state
-#A1 = Expression("0.0", degree=2)
-#A2 = Expression("0.0", degree=2)
-#T = Expression("1.0", degree=2)
-#U = Expression("1.0", degree=2)
-##Normal state
-#A1 = Expression("0.0", degree=2)
-#A2 = Expression("H*x[0]", H=H, degree=2)
-#T = Expression("0.0", degree=2)
-#U = Expression("0.0", degree=2)
-#PErturbed normal state
-A1 = Expression("H*x[1]", degree=2)
-A2 = Expression("H*x[0]", H=H, degree=2)
-T = Expression("x[1]-x[0]", degree=2)
-U = Expression("1.0", degree=2)
-#Vortex Solution.
-#..... Complete
-#---------------------------------------------------------------------------------------------------------------
-##Reading input from a .xdmf file.
-#a1a2u = Function(V)
-#a1 = Function(Vcoord)
-#a2 = Function(Vcoord)
-#t = Function(Vcoord)
-#u = Function(Vcoord)
-#a1_in =  XDMFFile("GL-2DEnrg-0.xdmf")
-#a1_in.read_checkpoint(a1,"a1",0)
-#a2_in =  XDMFFile("GL-2DEnrg-1.xdmf")
-#a2_in.read_checkpoint(a2,"a2",0)
-#t_in =  XDMFFile("GL-2DEnrg-2.xdmf")
-#t_in.read_checkpoint(t,"t",0)
-#u_in =  XDMFFile("GL-2DEnrg-3.xdmf")
-#u_in.read_checkpoint(u,"u",0)
-#assign(a1a2tu,[a1,a2,t,u])
-##plot(u)
-##plt.title(r"$u(x)-b4$",fontsize=26)
-##plt.show()
+if read_in == 0: # We want to use the standard values.
+ #SC state
+ A1 = interpolate( Expression("0.0", degree=2), V)
+ A2 = interpolate( Expression("0.0", degree=2), V)
+ T = interpolate( Expression("1.0", degree=2), V)
+ U = interpolate( Expression("1.0", degree=2), V)
+ ##Modified normal state
+ #A1 = interpolate( Expression("0.0", degree=2), V)
+ #A2 = interpolate( Expression("H*x[0]", H=H, degree=2), V)
+ #T = interpolate( Expression("x[1]", degree=2), V)
+ #U = interpolate( Expression("x[0]", degree=2), V)
+ ##Vortex Solution.
+ #..... need to complete
+###---------------------------------------------------------------------------------------------------------------
+elif read_in == 1: # We want to read from xdmf files
+ #Reading input from a .xdmf file.
+ A1 = Function(V)
+ A2 = Function(V)
+ T = Function(V)
+ U = Function(V)
+ a1_in =  XDMFFile("GL-2DEnrg-0.xdmf")
+ a1_in.read_checkpoint(A1,"a1",0)
+ a2_in =  XDMFFile("GL-2DEnrg-1.xdmf")
+ a2_in.read_checkpoint(A2,"a2",0)
+ t_in =  XDMFFile("GL-2DEnrg-2.xdmf")
+ t_in.read_checkpoint(T,"t",0)
+ u_in =  XDMFFile("GL-2DEnrg-3.xdmf")
+ u_in.read_checkpoint(U,"u",0)
+ #plot(u)
+ #plt.title(r"$u(x)-b4$",fontsize=26)
+ #plt.show()
+else:
+ import sys
+ sys.exit("Not a valid input for read_in.")
 
 a1_up.vector()[:] = A1.vector()[:]
 a2_up.vector()[:] = A2.vector()[:]
@@ -135,9 +133,15 @@ for tt in range(NN):
  a2_up.vector()[:] = a2.vector()[:] - gamma*Fa2_vec[:]
  t_up.vector()[:] = t.vector()[:] - gamma*Ft_vec[:]
  u_up.vector()[:] = u.vector()[:] - gamma*Fu_vec[:]
- if ( norm(Fa1_vec) + norm(Fa2_vec) + norm(Ft_vec) + norm(Fu_vec) ) < tol:
+ #print(Fa1_vec.get_local()) # prints the vector.
+ #print(np.linalg.norm(np.asarray(Fa1_vec.get_local()))) # prints the vector's norm.
+ tol_test = np.linalg.norm(np.asarray(Fa1_vec.get_local()))\
+           +np.linalg.norm(np.asarray(Fa2_vec.get_local()))\
+           +np.linalg.norm(np.asarray(Ft_vec.get_local()))\
+           +np.linalg.norm(np.asarray(Fu_vec.get_local()))
+ print(tol_test)
+ if float(tol_test)  < tol :
   break
- #print(F_vec.get_local()) # prints the vector.
  
 
 ##Save solution in a .xdmf file
@@ -149,12 +153,12 @@ a1a2tu_out.write_checkpoint(a2, "a2", 0, XDMFFile.Encoding.HDF5, False) #false m
 a1a2tu_out.close()
 a1a2tu_out = XDMFFile('GL-2DEnrg-2.xdmf')
 a1a2tu_out.write_checkpoint(t, "t", 0, XDMFFile.Encoding.HDF5, False) #false means not appending to file
-a1a2tu_out = XDMFFile('GL-2DEnrg-2.xdmf')
+a1a2tu_out = XDMFFile('GL-2DEnrg-3.xdmf')
 a1a2tu_out.write_checkpoint(u, "u", 0, XDMFFile.Encoding.HDF5, False) #false means not appending to file
 a1a2tu_out.close()
 
 
-pie = assemble((1/(l*l))*((1-u**2)**2/2 + (1/kappa**2)*inner(grad(u), grad(u)) \
+pie = assemble((1/(lx*ly))*((1-u**2)**2/2 + (1/kappa**2)*inner(grad(u), grad(u)) \
                         + ( (a1-t.dx(0))**2 + (a2-t.dx(1))**2 )*u**2 \
                             + inner( curl(a1 ,a2-Ae), curl(a1 ,a2-Ae) ) )*dx )
 print("Energy density =", pie)
