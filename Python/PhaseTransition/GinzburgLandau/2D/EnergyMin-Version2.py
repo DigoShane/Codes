26e6cfd4e015b03a5f884b62746ea3ca498f0ff6
Shoham Sen <digo.sen16@gmail.com>
2024-03-19 11:56:36 -0500

    commit before chaning EnergyMin-Version2 to EnergyMin.

--- Python/PhaseTransition/GinzburgLandau/2D/EnergyMin-Version2.py
+++ Python/PhaseTransition/GinzburgLandau/2D/EnergyMin-Version2.py
@@ -1,6 +1,30 @@
-#This is the same code as the prev. Only difference is we want to define 
-#the variables as Coefficients here.
-
+#Here we solve the 2D Ginzbug Landau problem with an applied magnetic field.
+#Here we want to use Energy minimization method. We start off with Gradient Descent.
+#HEre a1 is \ve{A}\cdot e_1, a2 is \ve{A}\cdot e_2, u is u. However, \theta=t
+#------------------------------------------------------------------------------------------------------
+# For details on progress, visit the overleaf file:-
+#1. Overleaf. superconductivity-Pradeep+Liping/Z3-Coding.tex/Sec. Stochastic Energy minimization methods
+# /subsec. Gradient Descent in FEniCS/paragraph{Wrote a 2D Ginzburg LAndau Energy minimization code}
+#======================================================================================================
+#The way the Code works
+#1. The input to the code is:
+#   a. The external field
+#   b. The relaxation parameter
+#   c. The absolute tolerance
+#2. When reading from and writing into respective files,
+#   we are writing the lagrange multiplier as a constant function
+#   When reading the functions, we interpolate onto a space VAu.
+#======================================================================================================
+#Things to keep in mind about writing this code:-
+#1. Define a functoon to evaluate the curl
+#2. Define a rotation funciton.
+#3. HAve replace L with l throught.
+#4. All variables are lower case.
+#5. REdo the code by using Hn\cdot B\perp
+#6. Implement Nesterov acceleration, momentum, minibatch gradient descent and Noisy Gradient Descent.
+#7. put in initial conditions for vortex solution.
+#======================================================================================================
+#ISSUES WITH THE CODE:-
 
 import dolfin
 print(f"DOLFIN version: {dolfin.__version__}")
@@ -39,20 +63,18 @@ t_up = Function(Vcoord)
 u_up = Function(Vcoord)
 
 # Parameters
-gamma = float(0.01) # Learning rate.
-NN = int(input('Number of iterations?\n')) # Number of iterations
-Hin = input("External Magnetic field? ")
-H = Constant(Hin);
-tol_in = input("absolute tolerance? ")
-tol = float(tol_in);
+gamma = float(input('Learning rate?')) # Learning rate.
+NN = int(input('Number of iterations?')) # Number of iterations
+H = Constant(input("External Magnetic field? "));
+tol = float(input("absolute tolerance?"))
 Ae = H*x[0] #The vec pot is A(x) = Hx_1e_2
 
 
 def curl(a1,a2):
-    return a1.dx(0) - a2.dx(1)
+    return a2.dx(0) - a1.dx(1)
 
 #Defining the energy
-Pi = ((1-u**2)**2/2 + (1/kappa**2)*inner(grad(u), grad(u)) + ( (a1-t.dx(0))**2 + (a2-t.dx(1))**2 )*u**2 \
+Pi = ( (1-u**2)**2/2 + (1/kappa**2)*inner(grad(u), grad(u)) + ( (a1-t.dx(0))**2 + (a2-t.dx(1))**2 )*u**2 \
       + inner( curl(a1 ,a2-Ae), curl(a1 ,a2-Ae) ) )*dx
 
 #Defining the gradient
@@ -63,34 +85,32 @@ Fu = derivative(Pi, u)
 
 
 ##Setting up the initial conditions
-##SC state
-#A1 = interpolate( Expression("0.0", degree=2), Vcoord)
-#A2 = interpolate( Expression("0.0", degree=2), Vcoord)
-#T = interpolate( Expression("1.0", degree=2), Vcoord)
-#U = interpolate( Expression("1.0", degree=2), Vcoord)
-#Normal state
+#SC state
 A1 = interpolate( Expression("0.0", degree=2), Vcoord)
-A2 = interpolate( Expression("H*x[0]", H=H, degree=2), Vcoord)
-T = interpolate( Expression("x[1]", degree=2), Vcoord)
-U = interpolate( Expression("x[0]", degree=2), Vcoord)
-#Vortex Solution.
+A2 = interpolate( Expression("0.0", degree=2), Vcoord)
+T = interpolate( Expression("1.0", degree=2), Vcoord)
+U = interpolate( Expression("1.0", degree=2), Vcoord)
+##Modified normal state
+#A1 = interpolate( Expression("0.0", degree=2), Vcoord)
+#A2 = interpolate( Expression("H*x[0]", H=H, degree=2), Vcoord)
+#T = interpolate( Expression("x[1]", degree=2), Vcoord)
+#U = interpolate( Expression("x[0]", degree=2), Vcoord)
+##Vortex Solution.
 #..... need to complete
-#---------------------------------------------------------------------------------------------------------------
+###---------------------------------------------------------------------------------------------------------------
 ##Reading input from a .xdmf file.
-#a1a2u = Function(V)
-#a1 = Function(Vcoord)
-#a2 = Function(Vcoord)
-#t = Function(Vcoord)
-#u = Function(Vcoord)
+#A1 = Function(Vcoord)
+#A2 = Function(Vcoord)
+#T = Function(Vcoord)
+#U = Function(Vcoord)
 #a1_in =  XDMFFile("GL-2DEnrg-0.xdmf")
-#a1_in.read_checkpoint(a1,"a1",0)
+#a1_in.read_checkpoint(A1,"a1",0)
 #a2_in =  XDMFFile("GL-2DEnrg-1.xdmf")
-#a2_in.read_checkpoint(a2,"a2",0)
+#a2_in.read_checkpoint(A2,"a2",0)
 #t_in =  XDMFFile("GL-2DEnrg-2.xdmf")
-#t_in.read_checkpoint(t,"t",0)
+#t_in.read_checkpoint(T,"t",0)
 #u_in =  XDMFFile("GL-2DEnrg-3.xdmf")
-#u_in.read_checkpoint(u,"u",0)
-#assign(a1a2tu,[a1,a2,t,u])
+#u_in.read_checkpoint(U,"u",0)
 ##plot(u)
 ##plt.title(r"$u(x)-b4$",fontsize=26)
 ##plt.show()
@@ -113,8 +133,8 @@ for tt in range(NN):
  a2_up.vector()[:] = a2.vector()[:] - gamma*Fa2_vec[:]
  t_up.vector()[:] = t.vector()[:] - gamma*Ft_vec[:]
  u_up.vector()[:] = u.vector()[:] - gamma*Fu_vec[:]
- print(Fa1_vec.get_local()) # prints the vector.
- print(np.linalg.norm(np.asarray(Fa1_vec.get_local()))) # prints the vector's norm.
+ #print(Fa1_vec.get_local()) # prints the vector.
+ #print(np.linalg.norm(np.asarray(Fa1_vec.get_local()))) # prints the vector's norm.
  tol_test = np.linalg.norm(np.asarray(Fa1_vec.get_local()))\
            +np.linalg.norm(np.asarray(Fa2_vec.get_local()))\
            +np.linalg.norm(np.asarray(Ft_vec.get_local()))\
@@ -133,7 +153,7 @@ a1a2tu_out.write_checkpoint(a2, "a2", 0, XDMFFile.Encoding.HDF5, False) #false m
 a1a2tu_out.close()
 a1a2tu_out = XDMFFile('GL-2DEnrg-2.xdmf')
 a1a2tu_out.write_checkpoint(t, "t", 0, XDMFFile.Encoding.HDF5, False) #false means not appending to file
-a1a2tu_out = XDMFFile('GL-2DEnrg-2.xdmf')
+a1a2tu_out = XDMFFile('GL-2DEnrg-3.xdmf')
 a1a2tu_out.write_checkpoint(u, "u", 0, XDMFFile.Encoding.HDF5, False) #false means not appending to file
 a1a2tu_out.close()
 
---
+++ Python/PySCF/Ex-9.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# Author: Qiming Sun <osirpt.sun@gmail.com>
+#
+
+#Dirac equation with Hartree Fock.
+
+'''
+Dirac-Hartree-Fock without time-reversal Kramers pair
+'''
+
+from pyscf import gto, scf, lib
+
+mol = gto.M(
+    atom = '''
+Cl 0  0     0
+H  0  1.9   0''',
+    basis = 'ccpvdz',
+)
+mf = scf.DHF(mol)
+mf.kernel()
+
+#
+# Uncontract basis of Cl, keep basis of H contracted
+#
+mol = gto.M(
+    atom = '''
+Cl 0  0     0
+H  0  1.9   0''',
+    basis = {'Cl': 'unc-ccpvdz',
+             'H' : 'ccpvdz'},
+)
+lib.param.LIGHT_SPEED = 90.  # Change light speed globally
+
+mf = scf.DHF(mol)
+mf.kernel()
